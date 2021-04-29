a2dffbeb37131187c43e634bbeed33eeb263bad2
Myfanwy <myfanwy@github.com>
2021-04-29 11:52:49 -0700

    add cover letter

--- methods/cvpdata.R
+++ methods/cvpdata.R
@@ -12,43 +12,35 @@ b = StdCrvKey$StdCrvBeta_lnForm[i]
 
 d$ln_eDNA = artemis::cq_to_lnconc(d$Cq, a, b)
 
-ggplot(d, aes(x = Distance_m, y = Cq)) +
-  geom_jitter(
-    width = 0.25,
-    size = 1.2,
-    alpha = 0.5,
-    aes(color = Species),
-    show.legend = FALSE
-  ) +
-  geom_boxplot(
-    alpha = 0.5,
-    aes(group = Distance_m),
-    width = 1.1,
-    size = 0.48,
-    outlier.shape = NA
-  ) +
-  scale_color_fishpals() +
-  theme_report(inner_border = FALSE)
-
-table(d$TechRep)
-
 v40 = artemis::cq_to_lnconc(40.0, unique(a), unique(b))
 
-ggplot(d, aes(x = Distance_m, y = ln_eDNA)) +
+ggplot(d, aes(x = factor(Distance_m), y = ln_eDNA)) +
   geom_jitter(
-    width = 0.25,
-    size = 1.2,
+    size = 1,
     alpha = 0.5,
-    aes(color = Species),
+    shape = 1,
+    aes(color = factor(Volume_mL)),
+    position = position_jitterdodge(jitter.width = 0.05, 
+                                    dodge.width = 0.55
+                                    ),
     show.legend = FALSE
   ) +
   geom_boxplot(
-    alpha = 0.5,
-    aes(group = Distance_m),
-    width = 1.1,
-    size = 0.48,
+     alpha = 0.25,
+    aes(color = factor(Volume_mL),
+        fill = factor(Volume_mL)),
+    position = position_dodge(width = 0.5),
+    width = 0.25,
+    size = 0.4,
     outlier.shape = NA
   ) +
   geom_hline(aes(yintercept = v40), lty = 2, size = 0.45) +
-  scale_color_fishpals() +
-  theme_report(inner_border = FALSE)
+  scale_color_manual(values = c("gray45", "gray10")) +
+  scale_fill_manual(values = c("gray45", "gray10")) +
+
+  theme_report(inner_border = FALSE) +
+  theme(legend.position = "bottom") +
+  labs(x = "Distance (m) from live car",
+       y = "ln[eDNA]") +
+  guides(color = guide_legend(title = "Filtered volume (mL)"),
+         fill = guide_legend(title = "Filtered volume (mL)"))
